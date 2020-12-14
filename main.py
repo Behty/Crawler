e534c7a0c1a8a5409b3671af6ba01c1e87c5e2cb
Behty <therunaway.27@gmail.com>
2020-12-14 22:12:23 +0200

    Базовый интерфейс. Поиск по нескольким параметрам. Общая база данных.

--- main.py
+++ main.py
@@ -3,65 +3,50 @@ from tkinter import *
 from tkinter import ttk
 
 
-def tel_num_crawler():
-    '''Ищет абонента по номеру телефона'''
-    abonents = []
-    file_tel_num = int_tel_num.get()
-    excel_data_file = xlrd.open_workbook('./tellist.xlsx')
+def data_crawler():
+    response = []
+    request = input_data.get().lower()
+    excel_data_file = xlrd.open_workbook('hardData.xlsx')
     sheet = excel_data_file.sheet_by_index(0)
     row_number = sheet.nrows
     if row_number > 0:
-        for row in range(0, row_number):
-            if str(file_tel_num) == str(sheet.row(row)[2]).replace('number:','').replace('.0',''):
-                abonents.append(str(sheet.row(row)[1]).replace('text:','').replace("'", ''))
-                continue
-        if abonents != []:
-            tel_num_lbl.config(text='\n'.join(abonents))
+        for row in range(1, row_number):
+            if request in str(sheet.row(row)[0]).replace('text:','').replace("'", '').lower() or \
+            request in str(sheet.row(row)[1]).replace('text:','').replace("'", '').lower() or \
+            request in str(sheet.row(row)[3]).replace('number:','').replace(".0", '') or \
+            request in str(sheet.row(row)[4]).replace('text:','').replace("'", '').lower():
+                response.append('Отделение:  ' + str(sheet.row(row)[6]).replace('text:','').replace("'", '').replace('empty:', '<нет данных>'))
+                response.append('Должность:  ' + str(sheet.row(row)[5]).replace('text:','').replace("'", '').replace('empty:', '<нет данных>'))
+                response.append('ФИО:  ' + str(sheet.row(row)[0]).replace('text:','').replace("'", ''))
+                response.append('Логин:  ' + str(sheet.row(row)[1]).replace('text:','').replace("'", '').replace('empty:', '<нет данных>'))
+                response.append('ID:  ' + str(sheet.row(row)[2]).replace('text:','').replace("'", '').replace('empty:', '<нет данных>'))
+                response.append('Имя компьютера:  ' + str(sheet.row(row)[4]).replace('text:','').replace("'", '').replace('empty:', '<нет данных>'))
+                response.append('Кабинет:  ' + str(sheet.row(row)[7]).replace('text:','').replace("'", '').replace('empty:', '<нет данных>'))
+                response.append('Телефон:  ' + str(sheet.row(row)[3]).replace('number:','').replace(".0", '').replace('empty:', '<нет данных>'))
+                response.append('')
+        if response != []:
+            output_data_lbl.config(text='\n'.join(response))
         else:
-            tel_num_lbl.config(text='Совпадений не найдено')
-
-
-def ID_crawler():
-    '''Ищет ID по фамилии'''
-    file_tel_num = lastname.get()
-    excel_data_file = xlrd.open_workbook('./IDlist.xlsx')
-    sheet = excel_data_file.sheet_by_index(0)
-    row_number = sheet.nrows
-    if row_number > 0:
-        for row in range(0, row_number):
-            if str(file_tel_num) == str(sheet.row(row)[0]).replace('text:','').replace("'",''):
-                IDword_lbl.config(text=str(sheet.row(row)[2]).replace('text:','').replace("'",''))
-                break
-            else:
-                IDword_lbl.config(text='Совпадений не найдено')
+            output_data_lbl.config(text='Совпадений не найдено')
 
 
 root = Tk()
-root.title('Crawler v1.0')
-root.geometry('+300+300')
+root.title('Crawler v2.0')
+root.geometry('+300+200')
 root.resizable(False, False)
 
-#Поиск абонента по номеру телефона
-ttk.Label(text='Внутренний номер: ').grid(row=0, column=0, sticky=E, padx=10, pady=20)
-int_tel_num = ttk.Entry(width=15)
-int_tel_num.grid(row=0, column=1, sticky=W, pady=10)
+ttk.Label(text='Поиск: ', font=('Lucida Console', 11)).grid(row=0, column=0, sticky=E, padx=15, pady=20)
+input_data = ttk.Entry(width=15)
+input_data.grid(row=0, column=1, sticky=W, pady=10)
 
-ttk.Label(text='Абонент: ').grid(row=0, column=2, sticky=E, padx=10, pady=20)
-tel_num_lbl = ttk.Label(width=30)
-tel_num_lbl.grid(row=0, column=3, sticky=E, padx=10, pady=20)
+ttk.Label(text='Сотрудник: ', font=('Lucida Console', 11)).grid(row=0, column=2, sticky=E, padx=10, pady=20)
+output_data_lbl = ttk.Label(width=50, font=('Lucida Console', 10))
+output_data_lbl.grid(row=0, column=3, sticky=E, padx=10, pady=20)
 
-show_but = ttk.Button(text='Узнать', command=tel_num_crawler).grid(row=0, column=6, ipadx=10, pady=20, padx=50)
+style = ttk.Style(root)
+style.configure('TButton', font=('Lucida Console', 11))
+show_but = ttk.Button(text='Вывод', command=data_crawler, style='TButton').grid(row=0, column=6, ipady=10, ipadx=10, pady=20, padx=20)
 
-#Поиск ID по фамилии
-ttk.Label(text='Фамилия: ').grid(row=1, column=0, sticky=E, padx=10, pady=20)
-lastname = ttk.Entry(width=15)
-lastname.grid(row=1, column=1, sticky=W, pady=10)
+input_data.focus()
 
-ttk.Label(text='ID: ').grid(row=1, column=2, sticky=E, padx=10, pady=20)
-IDword_lbl = ttk.Label(width=30)
-IDword_lbl.grid(row=1, column=3, sticky=E, padx=10, pady=20)
-
-show_but = ttk.Button(text='Узнать', command=ID_crawler).grid(row=1, column=6, ipadx=10, pady=20, padx=50)
-
-int_tel_num.focus()
 root.mainloop()
